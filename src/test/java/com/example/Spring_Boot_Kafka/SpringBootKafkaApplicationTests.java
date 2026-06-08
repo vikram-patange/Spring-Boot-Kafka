cd156ea57b277b3a49a0ec7cd71b4f1c06c6b3a3
vikram patange <vikrampatange11@gmail.com>
2026-06-08 12:20:16 +0530

    first commit

---
+++ src/test/java/com/example/Spring_Boot_Kafka/SpringBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
